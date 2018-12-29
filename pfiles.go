2a901bc6559e52c39330f595b9bbb11b04aec2e9
a.kushman <a.kushman@gismeteo.ru>
2018-12-29 19:08:36 +0300

    #4. Add O_WRONLY + O_APPEND + O_LOCK, GetFileContents impl via bufio

--- pfiles.go
+++ pfiles.go
@@ -3,12 +3,13 @@ package pgo
 import (
 	"io/ioutil"
 	"os"
+	"bufio"
 )
 
 // const mapping php -> go
 const (
-	FileAppend = os.ModeAppend
-	LockEx     = os.ModeExclusive
+	FileAppend = os.O_APPEND
+	LockEx     = os.O_EXCL
 )
 
 // FileGetContents reads files, http requests streams
@@ -20,16 +21,41 @@ func FileGetContents(fileName string, args ...interface{}) (string, error) {
 
 	// write with both offset/maxLen
 	if argsLen == 2 && args[0].(int) > 0 && args[1].(int) > 0 {
+		offset := args[0].(int)
+		limit := args[1].(int)
 
+		reader := initReader(fileName)
+		_, err := reader.Discard(offset) // skipping an offset from user input
+
+		if err != nil {
+			panic(err)
+		}
+
+		buf := make([]byte, limit)
+		_, e := reader.Read(buf)
+
+		return string(buf), e
 	}
 
 	// write with offset
 	if argsLen == 1 && args[0].(int) > 0 {
+		buf := make([]byte, args[0].(int))
+		_, e := initReader(fileName).Read(buf)
 
+		return string(buf), e
 	}
 
 	data, err := ioutil.ReadFile(fileName)
 	return string(data), err
+}
+
+func initReader(fileName string) *bufio.Reader {
+	f, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	return bufio.NewReader(f)
 }
 
 // FilePutContents write files with offset/limit
@@ -37,7 +63,9 @@ func FileGetContents(fileName string, args ...interface{}) (string, error) {
 // flags[0] - flags how to put this data FileAppend | LockEx
 func FilePutContents(fileName, data string, flags ...interface{}) (int, error) {
 	if len(flags) > 0 && flags[0] != nil {
-		f, err := os.OpenFile(fileName, flags[0].(int), 0644)
+		f, err := os.OpenFile(fileName, flags[0].(int) | os.O_WRONLY, 0644)
+		defer f.Close()
+
 		if err != nil {
 			panic(err)
 		}
--- pfiles_test.go
+++ pfiles_test.go
@@ -5,19 +5,64 @@ import (
 	"pgo"
 )
 
+const (
+	strToWrite = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	fileName   = "example.txt"
+)
+
 func TestFileGetContents(t *testing.T) {
+	n, err := pgo.FilePutContents(fileName, strToWrite)
 
-}
+	if err != nil {
+		panic(err)
+	}
 
-func TestFilePutContents(t *testing.T) {
-	str := "foo bar baz"
-	n, err := pgo.FilePutContents("example.txt", str)
+	// reading full file with limit
+	str, er := pgo.FileGetContents(fileName, 0, n)
 
-	if n != len(str) {
+	if er != nil {
+		panic(er)
+	}
+
+	if len(str) != n {
 		t.Fatalf("want %d bytes of data, got %d", n, len(str))
 	}
 
+	// reading offset/limit
+	off := n / 3
+	lim := n/2 - off - 1
+
+	ss, er := pgo.FileGetContents(fileName, off, lim)
+
+	if er != nil {
+		panic(er)
+	}
+
+	if len(ss) != lim {
+		t.Fatalf("want %d bytes of data, got %d", n, len(str))
+	}
+}
+
+func TestFilePutContents(t *testing.T) {
+	// test write to file with append without options
+	n1, err := pgo.FilePutContents(fileName, strToWrite)
+
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	if n1 != len(strToWrite) {
+		t.Fatalf("want %d bytes of data, got %d", n1, len(strToWrite))
+	}
+
+	// test write to file with append option
+	n2, err2 := pgo.FilePutContents(fileName, strToWrite, pgo.FileAppend)
+
+	if err2 != nil {
+		t.Fatal(err2)
+	}
+
+	if n2 == n1*2 {
+		t.Fatalf("want %d bytes of data, got %d", n2, n1*2)
 	}
 }
