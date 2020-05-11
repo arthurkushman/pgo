22d6ab11b465025e8ab0da74ac58dfef8dfe9399
arthurkushman <zeusakm@gmail.com>
2020-05-11 12:31:27 +0300

    #44: Add Serialize/Unserialize methods based on gob

--- core.go
+++ core.go
@@ -1,6 +1,9 @@
 package pgo
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/gob"
 	"fmt"
 	"os"
 )
@@ -16,4 +19,33 @@ func printError(msg string, args ...interface{}) {
 	if os.Getenv("PGO_ENV") != "dev" {
 		os.Exit(1)
 	}
+}
+
+// Serialize encodes Go code entities to string for e.g.: later storage
+func Serialize(val interface{}) (string, error) {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	err := e.Encode(val)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(b.Bytes()), nil
+}
+
+// Unserialize decodes string back into Go code representation
+func Unserialize(val string, v interface{}) error {
+	by, err := base64.StdEncoding.DecodeString(val)
+	if err != nil {
+		return err
+	}
+
+	b := new(bytes.Buffer)
+	b.Write(by)
+	err = gob.NewDecoder(b).Decode(v)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- core_test.go
+++ core_test.go
@@ -1,10 +1,27 @@
 package pgo_test
 
 import (
-	"github.com/arthurkushman/pgo"
 	"testing"
+
+	"github.com/arthurkushman/pgo"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIsOk(t *testing.T) {
 	pgo.Date(123) // check passing wrong typed parameter
+}
+
+func TestSerialize(t *testing.T) {
+	m := make(map[int]string, 0)
+	m[0] = "abc"
+
+	str, err := pgo.Serialize(m)
+	assert.NoError(t, err)
+	assert.Equal(t, str, "Dv+BBAEC/4IAAQQBDAAACf+CAAEAA2FiYw==")
+
+	err = pgo.Unserialize(str, &m)
+	assert.NoError(t, err)
+	assert.Equal(t, m, map[int]string{
+		0: "abc",
+	})
 }
