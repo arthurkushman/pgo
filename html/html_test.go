083eed7f7eff67ae09f8a5fe145a9e78b87aa745
arthurkushman <zeusakm@gmail.com>
2021-03-11 19:15:44 +0300

    #52: Fix test for html.StripTags

--- html/html_test.go
+++ html/html_test.go
@@ -1,18 +1,18 @@
 package html_test
 
 import (
-	"pgo/html"
+	"github.com/arthurkushman/pgo/html"
 	"testing"
 )
 
-func TestStripTags(t *testing.T) {
-	tests := []struct {
-		in, out string
-	}{
-		{"<div>Lorem <span>ipsum dolor sit amet</span>, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua.</div>",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua."},
-	}
+var tests = []struct {
+	in, out string
+}{
+	{"<div>Lorem <span>ipsum dolor sit amet</span>, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua.</div>",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua."},
+}
 
+func TestStripTags(t *testing.T) {
 	for _, test := range tests {
 		if got := html.StripTags(test.in, []string{"a"}); got != test.out {
 			t.Fatalf("%q: want %q, got %q", test.in, test.out, got)
