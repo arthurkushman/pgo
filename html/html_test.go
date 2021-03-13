f0762e7d1fd6cbf10e28662e8a213faac2255b3e
arthurkushman <zeusakm@gmail.com>
2021-03-13 10:29:43 +0300

    #52: Extend html.StripTags tests

--- html/html_test.go
+++ html/html_test.go
@@ -6,15 +6,26 @@ import (
 )
 
 var tests = []struct {
-	in, out string
+	in, out  string
+	excluded []string
 }{
 	{"<div>Lorem <span>ipsum dolor sit amet</span>, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua.</div>",
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua."},
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua.",
+		[]string{"a"},
+	},
+	{"<div>Lorem <span>ipsum dolor sit amet</span>, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua.</div>",
+		"<div>Lorem <span>ipsum dolor sit amet</span>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>",
+		[]string{"div", "span"},
+	},
+	{"<div>Lorem <span>ipsum dolor sit amet</span>, consectetur adipiscing elit, sed do eiusmod <a href=\"http://example.com\">tempor incididunt</a> ut labore et dolore magna aliqua.</div>",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		[]string{},
+	},
 }
 
 func TestStripTags(t *testing.T) {
 	for _, test := range tests {
-		if got := html.StripTags(test.in, []string{"a"}); got != test.out {
+		if got := html.StripTags(test.in, test.excluded); got != test.out {
 			t.Fatalf("%q: want %q, got %q", test.in, test.out, got)
 		}
 	}
